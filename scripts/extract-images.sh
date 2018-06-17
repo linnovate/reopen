b1441f5e9567dcff6ea4815d9f272d66c50680e2
yosef <yosef@linnovate.net>
2018-06-17 13:30:27 +0300

    extract based on image: and ignore Value (values.yaml based)

--- scripts/extract-images.sh
+++ scripts/extract-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 # make a list of every image that is used anywhere in this folder
-find .. -name "*y*ml" -exec grep image {} \; |  
+find .. -name "*y*ml" -exec grep -e 'image:' {} \; | grep -v 'Values' |
 sed "s/.*image: \?//g" | 
 sort | uniq
