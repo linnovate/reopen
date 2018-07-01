8758ad3333e3c68502ffb025138e3e4e9724ac90
danuser2@weblink04 <danuser2@weblink04>
2018-07-01 14:19:31 +0300

    made script match standard env-name

--- scripts/deploy-chart.sh
+++ scripts/deploy-chart.sh
@@ -3,10 +3,7 @@
 chart=$1
 namespace=$2
 chart_path=../charts/$chart
+deploy_name="${namespace}-${chart}"
 
-helm delete --purge "${chart}-${namespace}" || echo "chart was not installed"
-cp $chart_path/values.$namespace.yaml $chart_path/values.yaml
-
-helm install --name "${chart}-${namespace}" --namespace "$namespace" $chart_path
-
-rm $chart_path/values.yaml
+helm delete --purge $deploy_name || echo "chart was not installed"
+helm install --wait --name "$deploy_name -f values.$namespace.yaml --namespace "$namespace" $chart_path
