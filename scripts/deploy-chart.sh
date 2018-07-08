b5f0161fef50e99d386b95440f020c7fec10ce1e
yosef <yosef@linnovate.net>
2018-07-08 15:42:01 +0300

    missing quote

--- scripts/deploy-chart.sh
+++ scripts/deploy-chart.sh
@@ -6,4 +6,4 @@ chart_path=../charts/$chart
 deploy_name="${namespace}-${chart}"
 
 helm delete --purge $deploy_name || echo "chart was not installed"
-helm install --wait --name "$deploy_name -f values.$namespace.yaml --namespace "$namespace" $chart_path
+helm install --wait --name "$deploy_name" -f values.$namespace.yaml --namespace "$namespace" $chart_path
